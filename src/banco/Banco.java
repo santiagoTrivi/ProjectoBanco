ebeedbbb25d3be8dbcbd412622381f947ce339b2
santiagoTrivi <santiagocarvajal103@gmail.com>
2022-11-23 18:40:54 -0400

    primer commit base de registro bancario

---
+++ src/banco/Banco.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package banco;
+import java.io.*;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Carvajal-Triviño
+ */
+public class Banco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/banco/ColaPersonas.java
@@ -0,0 +1,148 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banco;
+import java.io.*;
+import java.util.Scanner;
+/**
+ *
+ * @author Carvajal-Triviño
+ */
+class persona{
+    private String nombre, apellido;
+    private int cedula;
+    private String solicitud;
+    private boolean prioridad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getSolicitud() {
+        return solicitud;
+    }
+
+    public void setSolicitud(String solicitud) {
+        this.solicitud = solicitud;
+    }
+
+    public boolean isPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(boolean prioridad) {
+        this.prioridad = prioridad;
+    }      
+}
+class Nodo{
+    private Nodo siguiente;
+    private persona cliente;
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public persona getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(persona cliente) {
+        this.cliente = cliente;
+    }
+}
+public class ColaPersonas {
+        private Nodo inicio, finalc;
+    void encolar(persona p){
+        Nodo nodo = new Nodo();
+        nodo.setCliente(p);
+        if(inicio == null){
+            inicio = finalc = nodo;  
+        }else{
+            finalc.setSiguiente(nodo);
+            finalc = finalc.getSiguiente();
+        }   
+    }
+    Nodo decencolar(){
+        if(this.isEmpty()){
+            System.out.println("No hay nadie en la cola");
+            return null;
+        }else{
+            Nodo temp = inicio;
+            inicio = inicio.getSiguiente();
+            temp.setSiguiente(null);
+            if(inicio == null) finalc = null;
+            return temp;
+        }
+    }
+    Nodo decencolarPriori(){
+        Nodo prioridad = null;
+        if (this.isEmpty()) {
+            System.out.println("la cola esta vacia");
+            return null;
+        }else{
+            ColaPersonas colaAux = new ColaPersonas();
+            while(!this.isEmpty() && prioridad != null){
+                Nodo temp = this.decencolar();
+                if (temp.getCliente().isPrioridad()) {
+                    prioridad = temp;
+                }else{
+                    colaAux.encolar(temp.getCliente()); //queueAux.enqueue(temp.valor, temp.prioridad);
+                }
+            }
+            if (prioridad == null) {
+                prioridad = colaAux.decencolar(); //queueAux.dequeue();
+            }
+            while(!this.isEmpty()){
+                Nodo temp = this.decencolar();
+                colaAux.encolar(temp.getCliente());
+                //queueAux.enqueue(temp.valor, temp.prioridad);
+            }
+            this.inicio = colaAux.inicio; //queueAux.front;
+            this.finalc = colaAux.finalc; //queueAux.rear;
+            return prioridad;
+        }    
+    }        
+    
+    boolean isEmpty(){
+        return inicio == null;
+    }
+    void cargarDocumento() throws IOException{
+        Scanner file = new Scanner(new File("clientes.txt"));
+        while(file.hasNext()){
+            String[] datos; // nombre, appelido, cedula, solicitud, prioridad
+            datos = file.nextLine().split(";");
+            persona cliente = new persona();
+            cliente.setNombre(datos[0]);
+            cliente.setApellido(datos[1]);
+            cliente.setCedula(Integer.parseInt(datos[2]));
+            cliente.setSolicitud(datos[3]);
+            cliente.setPrioridad(Boolean.parseBoolean(datos[4]));
+            this.encolar(cliente);
+        }
+    }
+}
