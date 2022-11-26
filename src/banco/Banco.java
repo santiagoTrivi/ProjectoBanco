49dbf12c0434c3849e5aa131188ffffeceffc27a
estefania garcia <stfnag>
2022-11-26 15:49:25 -0400

    Se agrego el metodo guardarpendientes, un while para que pasen 4 cientes normales y 1 prioridad, y por ultimo se calculo cuantos clientes van apasar diario Luis Azocar y Estefania Garcia

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -15,35 +15,51 @@ public class Banco {
     
         LocalTime hora = LocalTime.of(8, 0);
         LocalTime horaCierre = LocalTime.of(16, 0);
-        int comparacion = hora.compareTo(horaCierre);
         Cola colaprioridad = new Cola(Boolean.TRUE);
         Cola cola  = new Cola(Boolean.FALSE); 
         
-        while(comparacion < 0){
-            String x = cola.decencolar().getCliente().getSolicitud();
-            switch (x){
+        Integer atendidos = 0;
+        while(hora.isBefore(horaCierre)){
+            if (colaprioridad.isEmpty() && cola.isEmpty()) {
+                System.out.println("Todos los clientes fueron atendidos, restan horas de trabajo: " + Math.abs(horaCierre.minusHours(hora.getHour()).getHour()));
+                break;
+            }
+            
+            String x;
+            if (atendidos > 4) {
+                if (colaprioridad.isEmpty()) {
+                    continue;
+                }
+                x = colaprioridad.decencolar().getCliente().getSolicitud();
+                atendidos = 0;
+            } else {
+                if (cola.isEmpty()) {
+                    atendidos++;
+                    continue;
+                }
+                x = cola.decencolar().getCliente().getSolicitud(); 
+            }
+
+            switch (x.toLowerCase()){
                 case "consulta":
-                    hora = hora.plusMinutes(1);
-                    comparacion = hora.compareTo(horaCierre);
+                    hora = hora.plusMinutes(1).plusSeconds(30);
                 break;
                 case "pago":
                     hora = hora.plusMinutes(2);
-                    comparacion = hora.compareTo(horaCierre);
                 break;
                 case "deposito":
                     hora = hora.plusMinutes(3);
-                    comparacion = hora.compareTo(horaCierre);
                 break;
                 case "retiro":
                     hora = hora.plusMinutes(4);
-                    comparacion = hora.compareTo(horaCierre);
                 case "actualizacion":
                     hora = hora.plusMinutes(5);
-                    comparacion = hora.compareTo(horaCierre);
                 break;
             }
+            atendidos++;
         } 
-        
+        cola.guardarPendientes();
+        colaprioridad.guardarPendientes();
     }
 }
 
--- src/banco/Cola.java
+++ src/banco/Cola.java
@@ -12,20 +12,21 @@ public class Cola {
     void encolar(Persona k) throws IOException {
         Nodo nodo = new Nodo();
         nodo.setCliente(k);
-        if(this.inicio == null){
-            this.inicio = this.finalc = nodo;  
-        }else{
-            this.finalc.setSiguiente(nodo);
-            this.finalc = this.finalc.getSiguiente();
-        }   
-        System.out.println(k.toString());
+        nodo.setSiguiente(null);
+        if(this.inicio == null && this.finalc == null){
+            this.inicio = nodo;
+            this.finalc = nodo;  
+        }
+        
+        this.finalc.setSiguiente(nodo);
+        this.finalc = this.finalc.getSiguiente();
     }
     
     public Cola(Boolean pri) throws IOException {
         this.prioridad = pri;
         this.inicio = null;
         this.finalc = null;   
-        this.cargarDocumento();
+        this.cargarDocumento(new String[]{"clientesPendientes.txt", "clientes.txt"});
     }
  
     Nodo decencolar(){
@@ -36,8 +37,11 @@ public class Cola {
             Nodo temp = this.inicio;
             this.inicio = this.inicio.getSiguiente();
             temp.setSiguiente(null);
+                System.out.println("Hola");
+
             return temp;
         }
+
     }
   
     boolean isEmpty(){
@@ -45,24 +49,40 @@ public class Cola {
     }
     
         
-      void cargarDocumento() throws IOException{
-       // Scanner fileP = new Scanner(new File("clientesPendientes"));//
-        Scanner file = new Scanner(new File("clientes.txt"));
-      // this.action(fileP);//
-        while(file.hasNextLine()){
-            String[] datos; // nombre, appelido, cedula, solicitud, prioridad}
-            datos = file.nextLine().split(";");
-            Persona cliente = new Persona();
-            cliente.setNombre(datos[0]);
-            cliente.setApellido(datos[1]);
-            cliente.setCedula(datos[2]);
-            cliente.setSolicitud(datos[3]);
-            cliente.setPrioridad(Boolean.parseBoolean(datos[4]));
-            
-            if (this.prioridad == cliente.isPrioridad()) {
-                this.encolar(cliente);
-            }
-            
+    void cargarDocumento(String[] archivos) throws IOException{
+        Scanner file = null;
+        for (int i=0;i<archivos.length;i++)
+           // Scanner fileP = new Scanner(new File("clientesPendientes"));//
+                file = new Scanner(new File(archivos[i]));
+          // this.action(fileP);//
+            while(file.hasNextLine()){
+                String[] datos; // nombre, apelido, cedula, solicitud, prioridad}
+                datos = file.nextLine().split(";");
+                Persona cliente = new Persona();
+                cliente.setNombre(datos[0]);
+                cliente.setApellido(datos[1]);
+                cliente.setCedula(datos[2]);
+                cliente.setSolicitud(datos[3]);
+                cliente.setPrioridad(Boolean.parseBoolean(datos[4]));
+
+                if (this.prioridad == cliente.isPrioridad()) {
+                    this.encolar(cliente);
+                }
+        }
+    }
+    
+    void guardarPendientes() throws IOException {
+
+        String archivo = "clientesPendientes.txt";
+        while(!this.isEmpty()) {
+            Persona cliente = null;
+            cliente = this.decencolar().getCliente();
+                try ( FileWriter fw = new FileWriter(archivo, true);  BufferedWriter bw = new BufferedWriter(fw);  PrintWriter out = new PrintWriter(bw)) {
+                    out.println(cliente.toString());
+                } catch (IOException e) {
+                //exception handling left as an exercise for the reader
+                }
+           // }
         }
     }
 }
