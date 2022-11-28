73016a1d71f76ee4d8b3652747a92281071e4a90
santiagoTrivi <santiagocarvajal103@gmail.com>
2022-11-28 18:24:23 -0400

    Santiago: cambios de la pila y modificacion del switch

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -17,6 +17,12 @@ public class Banco {
         LocalTime horaCierre = LocalTime.of(16, 0);
         Cola colaprioridad = new Cola(Boolean.TRUE);
         Cola cola  = new Cola(Boolean.FALSE); 
+        PilaR registroAct = new PilaR();
+        PilaR registroRet = new PilaR();
+        PilaR registroDep = new PilaR();
+        PilaR registroPag = new PilaR();
+        PilaR registroCon = new PilaR();
+
         
         Integer atendidos = 0;
         while(hora.isBefore(horaCierre)){
@@ -25,41 +31,69 @@ public class Banco {
                 break;
             }
             
-            String x;
+            Nodo x;
             if (atendidos > 4) {
                 if (colaprioridad.isEmpty()) {
                     continue;
                 }
-                x = colaprioridad.decencolar().getCliente().getSolicitud();
+                x = colaprioridad.decencolar();
+                switch (x.getCliente().getSolicitud().toLowerCase()){
+                case "consulta":
+                    hora = hora.plusMinutes(1).plusSeconds(50);
+                    registroCon.insertar(x.getCliente());
+                break;
+                case "pago":
+                    hora = hora.plusMinutes(2);
+                    registroPag.insertar(x.getCliente());
+                break;
+                case "deposito":
+                    hora = hora.plusMinutes(3);
+                    registroDep.insertar(x.getCliente());
+                break;
+                case "retiro":
+                    hora = hora.plusMinutes(4);
+                    registroRet.insertar(x.getCliente());
+                case "actualizacion":
+                    hora = hora.plusMinutes(5);
+                    registroAct.insertar(x.getCliente());
+                break;
+            }
                 atendidos = 0;
             } else {
                 if (cola.isEmpty()) {
                     atendidos++;
                     continue;
                 }
-                x = cola.decencolar().getCliente().getSolicitud(); 
-            }
-
-            switch (x.toLowerCase()){
+                x = cola.decencolar();
+                switch (x.getCliente().getSolicitud().toLowerCase()){
                 case "consulta":
-                    hora = hora.plusMinutes(1).plusSeconds(30);
+                    hora = hora.plusMinutes(1).plusSeconds(50);
+                    registroCon.insertar(x.getCliente());
                 break;
                 case "pago":
                     hora = hora.plusMinutes(2);
+                    registroPag.insertar(x.getCliente());
                 break;
                 case "deposito":
                     hora = hora.plusMinutes(3);
+                    registroDep.insertar(x.getCliente());
                 break;
                 case "retiro":
                     hora = hora.plusMinutes(4);
+                    registroRet.insertar(x.getCliente());
                 case "actualizacion":
                     hora = hora.plusMinutes(5);
+                    registroAct.insertar(x.getCliente());
                 break;
             }
+            }
+
+            
             atendidos++;
         } 
         cola.guardarPendientes();
         colaprioridad.guardarPendientes();
+        registroCon.guardarRegistro();
     }
 }
 
---
+++ src/banco/PilaR.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banco;
+import java.io.*;
+/**
+ *
+ * @author Carvajal-Triviño
+ */
+class NodoP{
+    private Persona clienteA; 
+    private NodoP down;
+
+    public Persona getClienteA() {
+        return clienteA;
+    }
+
+    public void setClienteA(Persona clienteA) {
+        this.clienteA = clienteA;
+    }
+
+    public NodoP getDown() {
+        return down;
+    }
+
+    public void setDown(NodoP down) {
+        this.down = down;
+    }
+    
+}
+public class PilaR {
+    private NodoP top;
+
+    void insertar(Persona k){
+        NodoP nodoP = new NodoP();
+        nodoP.setClienteA(k);
+        if(top == null){
+            top = nodoP;
+        }else{
+            nodoP.setDown(top);
+            this.top = nodoP;
+        }
+    }
+    boolean isEmpty(){
+        return this.top == null; 
+    }
+    NodoP pop(){
+        NodoP temp;
+        if (this.isEmpty()){
+            System.out.println("Pila Vacia");
+            return null;
+        }else{
+            temp = this.top;
+            this.top = this.top.getDown();
+            temp.setDown(null);
+            return temp;
+        }
+    }
+         void guardarRegistro() throws IOException{
+            File verificacion = new File("Taquilla.txt");
+                    if(verificacion.exists()){
+                        String archivoPasado = "Taquilla 28 11 2022.txt";
+                        verificacion.renameTo(new File (archivoPasado));
+                    }
+         while(!this.isEmpty()){
+            NodoP aux = this.top;
+            String archivo = "Taquilla.txt";
+            try(FileWriter fw = new FileWriter(archivo, true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    PrintWriter out = new PrintWriter(bw))
+                {
+                        out.println(aux.getClienteA().toString());
+                    
+                } catch (IOException e) {
+                    //exception handling left as an exercise for the reader
+                }
+            this.pop();
+        }
+           
+    }
+        
+        
+}
+
