bedb5cc6a32d2047bbba2dc4ffe9d892635db847
santiagoTrivi <santiagocarvajal103@gmail.com>
2022-11-29 19:59:21 -0400

    Santiago Trivino: creacion de los metodos de la clase procesoRegistro

--- src/banco/ProcesoRegistro.java
+++ src/banco/ProcesoRegistro.java
@@ -3,11 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package banco;
+import java.io.*;
 
 /**
  *
  * @author Carvajal-Triviño
  */
+// 4.actualizacion: 5 min
+// 3.retiro: 4 min
+// 2.deposito: 3 min
+// 1.pago de servicios: 2 min
+// 0.consulta: 1.5 min
 public class ProcesoRegistro {
-    
+    PilaR conjuntoProceso[];
+
+    public ProcesoRegistro() {
+        for (int i = 0; i <= 4; i++) {
+            PilaR proceso = new PilaR();
+            conjuntoProceso[i] = proceso;
+        }
+        
+       
+    }
+
+    void alcacenamiento() throws IOException{
+        for (int i = 0; i <= 4; i++) {
+            conjuntoProceso[i].guardarRegistro();
+        }
+    }
 }
